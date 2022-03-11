355370e7a0cd952f39084556b0c733c8fbf7ee59
Cassio Santana <cassioo.roger@gmail.com>
2022-03-11 11:16:42 -0300

    Getting file path information

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a folder path: ");
+		String stringPath = sc.nextLine();
+		
+		File path = new File(stringPath);
+		
+		System.out.println("getPath: " + path.getPath());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getName: " + path.getName());
+		
+		sc.close();
+	}
+}
